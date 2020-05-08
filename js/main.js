0eaeaf097cc72b0bd537245e2cf9fb5200656048
TannerLarson <64563955+TannerLarson@users.noreply.github.com>
2020-05-08 11:01:56 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+(function ($) {
+	"use strict";
+	$('.column100').on('mouseover',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).addClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).addClass('hov-column-head-'+ verTable);
+	});
+
+	$('.column100').on('mouseout',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
+    });
+
+    $("#scroll1").scroll(function () {
+        $("#scroll2").scrollLeft($("#scroll1").scrollLeft());
+    });
+
+    $("#scroll2").scroll(function () {
+        $("#scroll1").scrollLeft($("#scroll2").scrollLeft());
+    });
+    
+
+})(jQuery);
